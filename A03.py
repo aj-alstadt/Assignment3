35afbcd6c17567f0a7db99a7b5e27881166f5bad
aj-alstadt <aj.alstadt@gmail.com>
2025-02-16 18:55:08 -0800

    getting closer

--- A03.py
+++ A03.py
@@ -1,4 +1,4 @@
-#*****************************************************************************
+# *****************************************************************************
 # Author:       	AJ Alstadt
 # Assignment:       Assignment 2
 # Date:         	2/16/2025
@@ -10,53 +10,63 @@
 # Sources:      	D2L Unit 6 content
 #
 # Notes:            Created in Pycharm, version control with Git
-#*****************************************************************************
+# *****************************************************************************
 
 
 def main():
+    # Welcome message
     print("\nWelcome to the VGRS (Video Game Rating System).\n\n"
           "The VGRS will allow you to rate the video games you’ve played, "
           "and then display the \nlist of games and their corresponding ratings,"
           " as well as some statistics.")
 
-    #Declaring variables
+    # Declaring variables, parallel lists
     titles = []
     scores = []
     totals = 0.00
     more = "y"
-
+    #Setting the min and max scores to the 1st cell so I can change them later
+    lowest = scores[0]
+    highest = scores[0]
 
     while more.lower() == "y":
 
         title = input("\nPlease enter the name of a game: ")
 
-        score = float(input("\nPlease rate the game from 1.00-10.00: "))
+        # Error Handling
+        if title == "":
+            print("You didn't enter a title.")
+            continue
 
-        #Store input in lists
+        # Ask for a score
+        while True:
+                score_input = input("\nPlease rate the game from 1.00-10.00: ")
+                #convert score to float
+                score = float(score_input)
+
+                # Error handling
+                if 1.00 <= score <= 10.00:
+                    break
+                else:
+                    print("The rating must be between 1.00 and 10.00. Please try again.")
+
+        # Store valid input in lists
         titles.append(title)
         scores.append(score)
 
         more = input("Would you like to enter another title? (Y/N): ")
 
-
-    #Adds the scores together to get the average later
+    # Adds the scores together to get the average later
     for i in range(len(scores)):
-       totals += scores[i]
+        totals += scores[i]
 
     average = totals / len(scores)
 
-    print(titles, scores, average )
 
-
-
-
-
-
-
-
-
-
-
+    print("\nHere is the summary of your ratings:")
+    print(f"Games: {titles}")
+    print(f"Scores: {scores}")
+    print(f"Average Rating: {average:.2f}")
 
 
 main()
