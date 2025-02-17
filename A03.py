566941eb92941c2270d2b00efa39256b80200b31
aj-alstadt <aj.alstadt@gmail.com>
2025-02-16 16:02:49 -0800

    Created header

---
+++ A03.py
@@ -0,0 +1,13 @@
+#*****************************************************************************
+# Author:       	AJ Alstadt
+# Assignment:       Assignment 2
+# Date:         	2/16/2025
+# Description:  	Video game rating program. This program will allow you
+# to enter video games you've played and assign them a rating from 1-10. Then
+# it will average the scores and tell you the minimum and maximum ratings.
+# Input:        	Video games, Ratings from 1-10
+# Output:       	List of all games, average score, minimum & max scores.
+# Sources:      	D2L Unit 6 content
+#
+# Notes:            Created in Pycharm, version control with Git
+#*****************************************************************************
